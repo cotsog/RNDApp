289e9e7a1289463b07f90458ee20d21dabad2b19
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2016-09-10 16:53:56 +0530

    Added xcode project

---
+++ RNDApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RNDApp
+//
+//  Created by Nitesh Meshram on 9/7/16.
+//  Copyright © 2016 V2Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
